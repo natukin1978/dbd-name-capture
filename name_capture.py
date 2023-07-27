447d2a53b9634c13785242244ed9663782a8b638
natukin1978 <natukin1978@hotmail.com>
2023-07-27 20:20:06 +0900

    The resolution was changed, so I adjusted the size of the image

--- name_capture.py
+++ name_capture.py
@@ -8,6 +8,16 @@ import cv2  # OpenCVライブラリ
 
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
+
+def resize_screenshot(image_path, target_width, target_height):
+    screenshot = cv2.imread(image_path)
+
+    # 画像のリサイズ
+    resized_screenshot = cv2.resize(screenshot, (target_width, target_height))
+
+    return resized_screenshot
+
+
 args = len(sys.argv)
 if args <= 1:
     print("dbd player name", file=sys.stderr)
@@ -17,12 +27,15 @@ if args <= 1:
 
 img_folder = sys.argv[1]
 
+TARGET_WIDTH = 1536
+TARGET_HEIGHT = 864
+
 list_images = glob.glob(os.path.join(img_folder, "*.png"))
 for image in list_images:
     # スクリーンショットの読み込み
-    screenshot = cv2.imread(image)
+    screenshot = resize_screenshot(image, TARGET_WIDTH, TARGET_HEIGHT)
 
-    # プレイヤー名の表示座標 (例)
+    # プレイヤー名の表示座標
     x = 172
     y = 222
     width = 400
@@ -38,7 +51,7 @@ for image in list_images:
         gray = cv2.cvtColor(player_name_region, cv2.COLOR_BGR2GRAY)
 
         # 画像の前処理（必要に応じて調整）
-        # 例: 画像の二値化
+        # 画像の二値化
         threshold = cv2.threshold(
             gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU
         )[1]
